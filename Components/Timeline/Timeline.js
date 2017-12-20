82837df2d571fab54bd620af1b818e328a25262c
Dhairya <dhairyagada1997@gmail.com>
2017-12-20 20:47:25 +0530

    Cards Experiment

--- Components/Timeline/Timeline.js
+++ Components/Timeline/Timeline.js
@@ -3,11 +3,13 @@ import {AppRegistry, Text, View} from 'react-native';
 import { Container, Content,Header,Footer } from "native-base";
 import Timeline_Head from '../Timeline_Head/Timeline_Head';
 import Timeline_Footer from '../Timeline_Footer/Timeline_Footer';
+import Timeline_Cards from '../Timeline_Cards/Timeline_Cards';
 export default class Timeline extends Component{
     render(){
         return(
           <Container>   
         <Timeline_Head/>
+        <Timeline_Cards/>
         <Timeline_Footer/>
         </Container>
         );
