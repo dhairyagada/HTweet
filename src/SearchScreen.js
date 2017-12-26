34f0967e490f7f5ce84b0e7df9c6318d7c59fae3
Dhairya <dhairyagada1997@gmail.com>
2017-12-26 13:09:56 +0530

    Search Screen Added SearchInput and Body Color

--- src/SearchScreen.js
+++ src/SearchScreen.js
@@ -1,17 +1,20 @@
 import React,{Component} from 'react';
-import {Container} from 'native-base';
+import {Container,Header,Input,Item,Button,Text,Content} from 'native-base';
 export default class SearchScreen extends Component
 {
     render()
     {
-        return
-        (
+        return(
+        
             <Container>
-                <Header Searchbar>
-                <Input placeholder="Search Twitter" />
+                <Header searchBar style={{backgroundColor:'#F0F3F4',marginTop:25}}>
+                    <Item>
+                        <Input placeholder="Search Twitter" style={{backgroundColor:'#F0F3F4'}} />
+                    </Item>
                 </Header>
 
-                
+                <Content style={{backgroundColor:'#AED6F1'}}>
+                </Content>
             </Container>
         );
     }
